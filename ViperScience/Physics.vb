46bdafb8367c01a7c71fcda43c9595d736962c2b
Roy Banuelos <spinosaurus001@hotmail.com>
2016-08-06 00:25:17 -0700

    applied spacing to physics

--- ViperScience/Physics.vb
+++ ViperScience/Physics.vb
@@ -5,22 +5,27 @@
     Public Function consAcc_FVIVT(velocity As Double, time As Double) As Double
         Return consAcc_FITVA(velocity, time, 9.8)
     End Function
+
     ' initial velocity from final velocity with time
     Public Function consAcc_IVFVT(finalVel As Double, time As Double) As Double
         Return consAcc_IVFVTA(finalVel, time, 9.8)
     End Function
+
     ' Time from inital and final velocity
     Public Function consAcc_TIVFV(finalVel As Double, initVel As Double) As Double
         Return consAcc_TIVFV(finalVel, initVel, 9.8)
     End Function
+
     ' final velocity from initial and time (varying acceloration)
     Public Function consAcc_FITVA(velocity As Double, time As Double, acc As Double) As Double
         Return velocity + (acc * time)
     End Function
+
     ' initial velocity from final velocity with time (varying acceloration)
     Public Function consAcc_IVFVTA(finalVel As Double, time As Double, acc As Double) As Double
         Return finalVel - acc * time
     End Function
+
     ' Time from inital and final velocity
     Public Function consAcc_TIVFV(finalVel As Double, initVel As Double, acc As Double) As Double
         Return (finalVel - initVel) / acc
